8988827bed692568b404881d853ea1689b3d69d2
umerhasan17 <umerhasan17@gmail.com>
2019-01-01 18:15:55 +0000

    Adding a gitignore and defining a pullData function

--- stockAnalysis1/main.py
+++ stockAnalysis1/main.py
@@ -1,1 +1,30 @@
+# import urllib2
+import time
+import pandas as pd
+# import pandas_datareader.data as web
+import datetime
+
+stockToPull = 'AAPL'
+
+def pullData2(stock):
+    start = datetime.datetime(2010,1,1)
+    end = datetime.datetime(2019,1,1)
+    data = pd_datareader.data.DataReader(stock, yahoo, start, end)
+    return data
+
+
+# def pullData(stock):
+#     try:
+#         fileLine = stock+'.txt'
+#         urlToVisit = 'http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+'/chartdata;type=quote;range=1y/csv'
+#         sourceCode = urllib2.urlopen(urlToVisit).read()
+#         splitSource = sourceCode.split('\n')
+
+#         for eachLine in splitSource:
+#             splitLine = eachLine.split(',')
+
+
+#     except Exception, e:
+#         print 'main loop', str(e)
+
 print("Hello")
