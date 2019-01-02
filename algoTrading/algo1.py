1de6ab7653418645d7322abdbf7790e3655d4d99
umerhasan17 <umerhasan17@gmail.com>
2019-01-02 00:43:24 +0000

    simple 100ma

--- algoTrading/algo1.py
+++ algoTrading/algo1.py
@@ -27,9 +27,25 @@ df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0) # getting
 
 # GRAPHING
 
+# plot the dataframe
 # df.plot()
 # plot a single statistic
 # df['Adj Close'].plot()
+# plt.show() to show when running off command line
+# plt.show()
+
+# DATA MANIPULATION 100ma
+
+# 100 ma for stock
+df['100ma'] = df['Adj Close'].rolling(window=100).mean()
+# drop NaNs for the first 100 days of range
+df.dropna(inplace=True)
+
+# 2 sub graphs one displaying 2 lines on 1 graph with vol below it
+ax1 = plt.subplot2grid((6, 1), (0, 0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6, 1), (5, 0), rowspan=1, colspan=1, sharex = ax1)
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
 plt.show()
-
-
