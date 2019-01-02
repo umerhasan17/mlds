e17218a7b89c72133ea4a753abaf395f2ba5b887
umerhasan17 <umerhasan17@gmail.com>
2019-01-02 00:23:51 +0000

    creating a CSV file for a stock after getting data using pandas_dr

---
+++ algoTrading/algo1.py
@@ -0,0 +1,19 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+
+
+style.use('ggplot')
+
+start = dt.datetime(2000, 1, 1)
+end = dt.datetime(2019, 1, 1)
+
+df = web.DataReader('TSLA', 'yahoo', start, end)
+
+# print(df.head()) print first 5
+# print(df.tail()) print last 5
+
+df.to_csv('TSLA.csv')
