7cd1a55b24d270e93193e9d42b55b846cbf58506
umerhasan17 <umerhasan17@gmail.com>
2019-01-02 00:31:32 +0000

    simply IO + graphing stocks

--- algoTrading/algo1.py
+++ algoTrading/algo1.py
@@ -4,16 +4,32 @@ from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
 
-
-
 style.use('ggplot')
 
 start = dt.datetime(2000, 1, 1)
 end = dt.datetime(2019, 1, 1)
 
-df = web.DataReader('TSLA', 'yahoo', start, end)
+# INPUT/OUTPUT
 
-# print(df.head()) print first 5
-# print(df.tail()) print last 5
+# getting data from online
+# df = web.DataReader('TSLA', 'yahoo', start, end) 
 
-df.to_csv('TSLA.csv')
+# storing data in a csv
+# df.to_csv('tsla.csv') 
+
+# getting data from local csv
+df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0) # getting
+
+# print first 5
+# print(df.head()) 
+# print last 5
+# print(df.tail())
+
+# GRAPHING
+
+# df.plot()
+# plot a single statistic
+# df['Adj Close'].plot()
+plt.show()
+
+
