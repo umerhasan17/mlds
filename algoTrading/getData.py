bd1aba15752a1f6d0caf165f978e3426a49931b8
umerhasan17 <umerhasan17@gmail.com>
2019-01-03 14:03:24 +0000

    Getting all spy data and collating it in seperate files.

---
+++ algoTrading/getData.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pandas_datareader.data as web
+import pickle
+import datetime as dt
+import os
+
+start = dt.datetime(2000,1,1)
+end = dt.datetime(2019,1,1)
+
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+        tickers = save_sp500_tickers()
+    else:
+        with open ("sp500tickers.pickle", "rb") as f:
+            tickers = pickle.load(f)
+        
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+    
+    for ticker in tickers:
+        print(ticker)
+        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+            df = web.DataReader(ticker, 'yahoo', start, end)
+            df.to_csv('stock_dfs/{}.csv'.format(ticker))
+        else:
+            print('Already have {}'.format(ticker))
+        
+get_data_from_yahoo()
---
+++ algoTrading/manipulateData.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import pandas_datareader.data as web
+import pickle
+import datetime as dt
+import os
+
+def compile_data():
+    with open ("sp500tickers.pickle", "rb") as f:
+        tickers = pickle.load(f)
+    
+    main_df = pd.DataFrame()
+
+    for count, ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+        df.rename(columns= {'Adj Close', ticker}, inplace=True)
+        df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+        
+        if count % 10 == 0:
+            print(count)
+    
+    print(main_df.head())
+    main_df.tocsv('sp500_joined_closes.csv')
+
+compile_data()
