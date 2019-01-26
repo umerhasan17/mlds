f766bb21da021f395a09a52a9e2147555630bb0d
umerhasan17 <umerhasan17@gmail.com>
2019-01-26 20:41:51 +0000

    plotting

--- generalML/linearRegression.py
+++ generalML/linearRegression.py
@@ -6,6 +6,10 @@ import math
 import numpy as np
 from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -60,3 +64,22 @@ accuracy = clf.score(X_test, y_test)
 forecast_set = clf.predict(X_lately)
 
 print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan, for _ in range(len(df.columns) - 1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forcast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
