f817dc674afdaa74f9aff5572f95f011b640ea6a
umerhasan17 <umerhasan17@gmail.com>
2019-01-26 18:48:02 +0000

    35 days in advance direction accuracy

--- generalML/linearRegression.py
+++ generalML/linearRegression.py
@@ -3,6 +3,9 @@
 import pandas as pd
 import quandl
 import math
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -24,11 +27,27 @@ forecast_col = 'Adj. Close'
 df.fillna('-99999', inplace=True)
 
 forecast_out = int(math.ceil(0.01 * len(df)))
+print(forecast_out)
 
 # creating the label
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 
+# all our features is everything but label
+X = np.array(df.drop(['label'], 1))
+
+# skip in HFT
+X = preprocessing.scale(X)
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+# splitting up train and test
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+# 97% accuracy
 
 
-print (df.tail())
+print (accuracy)
