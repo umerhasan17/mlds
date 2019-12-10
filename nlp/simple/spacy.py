59dee8456a50523d7f929ecd67394e82c7892ad6
umerhasan17 <umerhasan17@gmail.com>
2019-12-10 23:08:12 +0000

    Issues with spacy

--- nlp/simple/spacy.py
+++ nlp/simple/spacy.py
@@ -1,10 +1,13 @@
-import spacy
-from spacy.lemmatizer import Lemmatizer
-
-lemmatizer = Lemmatizer()
-word_list = ['feet', 'foot', 'foots', 'footing']
-lemmatized = [lemmatizer.lookup(word) for word in word_list]
-print(lemmatized)
-
-
-print("Test")
+import spacy as spacy
+print(spacy)
+nlp = spacy.load("en_core_web_sm")
+# doc = nlp("yee yee yeeeeeet")
+# for token in doc:
+#     print(token.text, token.pos_, token.dep_)
+# from spacy.lemmatizer import Lemmatizer
+# from spacy.lemmatizer import Lemmatizer
+# from spacy.lookups import Lookups
+# lemmatizer = Lemmatizer()
+# word_list = ['feet', 'foot', 'foots', 'footing']
+# lemmatized = [lemmatizer.lookup(word) for word in word_list]
+# print(lemmatized)
