db095a1adebcf0e57dd5eb3a85d88d55e9e026d0
umerhasan17 <umerhasan17@gmail.com>
2020-01-16 23:02:14 +0000

    add simple naive bayes text classifier

--- nlp/simple/naive-bayes.py
+++ nlp/simple/naive-bayes.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 
 import numpy as np
+import pandas as pd
 import nltk
 from nltk import pos_tag
 from nltk.corpus import stopwords
@@ -14,5 +15,46 @@ from sklearn.preprocessing import LabelEncoder
 
 np.random.seed(500)
 
-Corpus = nltk.corpus.state_union.words()
+Corpus = pd.read_csv('../disaster-tweets.csv')
 
+print("Columns: ", Corpus.columns)
+
+# Step - a : Remove blank rows if any.
+Corpus['text'].dropna(inplace=True)
+# Step - b : Change all the text to lower case. This is required as python interprets 'dog' and 'DOG' differently
+Corpus['text'] = [entry.lower() for entry in Corpus['text']]
+# Step - c : Tokenization : In this each entry in the corpus will be broken into set of words
+Corpus['text']= [word_tokenize(entry) for entry in Corpus['text']]
+# Step - d : Remove Stop words, Non-Numeric and perfom Word Stemming/Lemmenting.
+# WordNetLemmatizer requires Pos tags to understand if the word is noun or verb or adjective etc. By default it is set to Noun
+tag_map = defaultdict(lambda : wn.NOUN)
+tag_map['J'] = wn.ADJ
+tag_map['V'] = wn.VERB
+tag_map['R'] = wn.ADV
+for index,entry in enumerate(Corpus['text']):
+    # Declaring Empty List to store the words that follow the rules for this step
+    Final_words = []
+    # Initializing WordNetLemmatizer()
+    word_Lemmatized = WordNetLemmatizer()
+    # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
+    for word, tag in pos_tag(entry):
+        # Below condition is to check for Stop words and consider only alphabets
+        if word not in stopwords.words('english') and word.isalpha():
+            word_Final = word_Lemmatized.lemmatize(word,tag_map[tag[0]])
+            Final_words.append(word_Final)
+    # The final processed set of words for each iteration will be stored in 'text_final'
+    Corpus.loc[index,'text_final'] = str(Final_words)
+
+print("Starting training")
+Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(Corpus['text_final'],Corpus['target'],test_size=0.3)
+
+print("Word vectorization")
+Tfidf_vect = TfidfVectorizer(max_features=5000)
+Tfidf_vect.fit(Corpus['text_final'])
+Train_X_Tfidf = Tfidf_vect.transform(Train_X)
+Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+
+Naive = naive_bayes.MultinomialNB()
+Naive.fit(Train_X_Tfidf,Train_Y)
+predictions_NB = Naive.predict(Test_X_Tfidf)
+print("Naive Bayes Accuracy Score -> ",accuracy_score(predictions_NB, Test_Y)*100)
